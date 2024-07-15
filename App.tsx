04e76825557932149403cef0d58575e18a260aba
Ashil Poojary <ashilpoojary22@gmail.com>
2024-07-15 23:01:36 +0530

    tailwind set up

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View clasName="flex-1 justifyContent alignItems">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
